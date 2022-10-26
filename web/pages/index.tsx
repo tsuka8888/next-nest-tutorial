7390ff746f7999fbdd478cbf06e92679f64d29be
tsuka8888 <tks.ohtsu.fw77@gmail.com>
2022-10-26 23:32:54 +0900

    next再作成

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,8 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
@@ -34,7 +33,7 @@ const Home: NextPage = () => {
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
             <h2>Examples &rarr;</h2>
@@ -43,6 +42,8 @@ const Home: NextPage = () => {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
@@ -68,5 +69,3 @@ const Home: NextPage = () => {
     </div>
   )
 }
-
-export default Home
